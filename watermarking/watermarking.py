30e61e4ccabe95b32d8347578b35e61b873a65d6
蔡銘峯 <parktasi@gmail.com>
2018-05-09 16:29:39 +0800

    Update watermarking.py

--- watermarking/watermarking.py
+++ watermarking/watermarking.py
@@ -1,3 +1,10 @@
+"""
+As long as you retain this notice 
+you can do whatever with this stuff ,just you want. 
+If we meet some day, and you think this stuff is worth it, 
+you can buy me a beer in return park tsai  and share your idea with me .
+Let us change the world
+"""
 import sys
 
 import os
